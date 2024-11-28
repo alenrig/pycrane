b8673b3bc6bfec8577672b2cabb3923140403d9a
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-11-28 20:27:11 +0300

    Add some attrs

--- src/pycrane/client.py
+++ src/pycrane/client.py
@@ -7,7 +7,13 @@ class Pycrane:
     """Represents a Registry server connection."""
 
     def __init__(
-        self, url: Optional[str] = None, authfile: Optional[str] = None
+        self,
+        url: Optional[str] = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        authfile: Optional[str] = None,
     ) -> None:
         self.url = url
+        self.username = username
+        self.password = password
         self.authfile = authfile
