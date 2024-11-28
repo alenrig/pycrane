5c54f5c7d71f12f351aa018094e82ce92712c506
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-11-28 21:58:36 +0300

    Update

--- src/pycrane/client.py
+++ src/pycrane/client.py
@@ -2,6 +2,8 @@
 
 from typing import Optional
 
+from requests.auth import AuthBase, HTTPBasicAuth
+
 
 class Pycrane:
     """Represents a Registry server connection."""
@@ -17,3 +19,17 @@ class Pycrane:
         self.username = username
         self.password = password
         self.authfile = authfile
+        self._set_auth_info()
+
+    def _set_auth_info(self) -> None:
+        if (self.username and not self.password) or (
+            not self.username and self.password
+        ):
+            raise ValueError("Both username and password should be defined")
+        if self.authfile and self.username:
+            raise ValueError(
+                "Only one of authfile or username should be defined"
+            )
+        self._auth: Optional[AuthBase] = None
+        if self.username and self.password:
+            self._auth = HTTPBasicAuth(self.username, self.password)
