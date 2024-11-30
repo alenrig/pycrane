a662fffc14cb4df7a2927ea8c5352686ccd36227
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-11-30 10:49:00 +0300

    Add tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,11 @@
+"""Client testing module."""
+
 import pytest
+
 from pycrane.client import Pycrane
 
 
 def test_client_init():
+    """Test client initialization."""
     with pytest.raises(ValueError):
         Pycrane()
