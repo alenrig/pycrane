0315c875c3edc5fe35abbf44ac6abff609f3d289
Aleksandr Gorokhov <agorokhov94@gmail.com>
2024-12-01 10:47:19 +0300

    Moidfy test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,58 @@
 """Client testing module."""
 
+from contextlib import nullcontext
+
 import pytest
 
 from pycrane.client import Pycrane
 
 
-def test_client_init():
+@pytest.mark.parametrize(
+    ["username", "password", "authfile", "expectation", "error_msg"],
+    [
+        (None, None, None, pytest.raises(ValueError), "Specify auth method"),
+        (
+            "a",
+            None,
+            None,
+            pytest.raises(ValueError),
+            "Both username and password should be defined",
+        ),
+        (
+            None,
+            "b",
+            None,
+            pytest.raises(ValueError),
+            "Both username and password should be defined",
+        ),
+        (
+            "a",
+            None,
+            "path",
+            pytest.raises(ValueError),
+            "Both username and password should be defined",
+        ),
+        (
+            None,
+            "b",
+            "path",
+            pytest.raises(ValueError),
+            "Both username and password should be defined",
+        ),
+        (
+            "a",
+            "b",
+            "path",
+            pytest.raises(ValueError),
+            "Only one of authfile or username should be defined",
+        ),
+        ("a", "b", None, nullcontext(), ""),
+        (None, None, "path", nullcontext(), ""),
+    ],
+)
+def test_client_init(username, password, authfile, expectation, error_msg):
     """Test client initialization."""
-    with pytest.raises(ValueError):
-        Pycrane()
+    with expectation as e:
+        Pycrane(username=username, password=password, authfile=authfile)
+    if e:
+        assert str(e.value) == error_msg
